739cbc0a4a22b4851d32b438bd35fd21e63971aa
sohailshaik765@gmail.com <sohailshaik765@gmail.com>
2021-02-05 14:36:32 +0530

    hm

--- beauty/beauty_page.js
+++ beauty/beauty_page.js
@@ -45,10 +45,6 @@ function bodyFilter(){
 
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a874ed085bf9aed382700f22d9a7160e9ee10f23
 function showData(){
     // let url = `https://nitrogen-project.herokuapp.com/beauty_page`
     fetch(`https://nitrogen-project.herokuapp.com/beauty_page`)
