02ea0e3caa818864321f8a7c6d3c4d6c1a03c1fd
ankur <ankurp31031@gmail.com>
2021-02-06 11:15:32 +0530

    login

--- footer/page.js
+++ footer/page.js
@@ -116,24 +116,3 @@ $(document).ready(function () {
         }
     })
 })
-
-
-
-// function goPage(){
-//     let inpValue = document.getElementById('mobileNoEnter').value
-//     inpValue = Number(inpValue)
-//     // console.log(inpValue)
-//     let flag = false
-//     for(let i=0;i<arrayOfNumber.length;i++){
-//         if(inpValue===arrayOfNumber[i]){
-//             flag = true
-//         }
-//     }
-//     if(flag===true){
-//         let hp = document.getElementById('home').href
-//         // alert(hp)
-//         location = hp
-//     }else{
-//         alert('Enter Valid Mobile Number or Check again')
-//     }
-// }
